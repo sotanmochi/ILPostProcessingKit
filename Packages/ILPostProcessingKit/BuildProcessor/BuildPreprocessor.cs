8b64010761a0cb4ceba309d7aed4a62ddca09fd0
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-11-28 02:12:07 +0900

    Update BuildPreprocessor

--- Packages/ILPostProcessingKit/BuildProcessor/BuildPreprocessor.cs
+++ Packages/ILPostProcessingKit/BuildProcessor/BuildPreprocessor.cs
@@ -1,44 +1,27 @@
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
 namespace Unity.ILPostProcessingKit.BuildProcessor
 {
-    public class BuildPreprocessor : IPreprocessBuildWithReport
+    public sealed class BuildPreprocessor : IPreprocessBuildWithReport
     {
-        public int callbackOrder => 0;
+        public static readonly string EnableProfilingBlockWeaver = "ENABLE_PROFILING_BLOCK_WEAVER";
+        public static readonly int CallbackOrder = 0;
 
-        private static readonly string EnableProfilingBlockWeaver = "ENABLE_PROFILING_BLOCK_WEAVER";
+        public int callbackOrder => CallbackOrder;
 
         public void OnPreprocessBuild(BuildReport report)
         {
             var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(report.summary.platform);
 
-            RemovePredefinedSymbol(buildTargetGroup, EnableProfilingBlockWeaver);
+            BuildProcessorHelpers.RemoveDefineSymbol(buildTargetGroup, EnableProfilingBlockWeaver);
 
             var isDevelopmentBuild = report.summary.options.HasFlag(BuildOptions.Development);
             if (isDevelopmentBuild)
             {
-                var predefinedSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, $"{predefinedSymbols};{EnableProfilingBlockWeaver}");
+                BuildProcessorHelpers.AddDefineSymbol(buildTargetGroup, EnableProfilingBlockWeaver);
             }
-        }
-
-        private void RemovePredefinedSymbol(BuildTargetGroup buildTargetGroup, string symbol)
-        {
-            var predefinedSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup).Split(';');
-
-            var newSymbols = new List<string>();
-            foreach (var s in predefinedSymbols)
-            {
-                if (s != symbol)
-                {
-                    newSymbols.Add(s);
-                }
-            }
-
-            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, string.Join(";", newSymbols));
         }
     }
 }
